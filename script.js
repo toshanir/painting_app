c5c560cd4a59959c71b90e1c3f5a0df0169366da
Toshani <172811701+toshanir@users.noreply.github.com>
2025-10-28 20:38:36 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,82 +1,47 @@
-window.onload = function () {
+.main-container {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+}
 
-  // Definitions
-  var canvas = document.getElementById("paint-canvas");
-  var context = canvas.getContext("2d");
-  var boundings = canvas.getBoundingClientRect();
-  var range = document.getElementById("brush").value;
+canvas {
+  border: 1px black solid;
+  cursor:crosshair;
+}
 
-  // Specifications
-  var mouseX = 0;
-  var mouseY = 0;
-  var isDrawing = false;
-  context.strokeStyle = 'black'; // initial brush color
+.colors button {
+  display: inline-block;
+  border: 1px solid #00000026;
+  border-radius: 70px;
+  outline: none;
+  cursor: pointer;
+  width: 20px;
+  height: 20px;
+  margin-bottom: 5px;
+}
 
-  //Handle brush size
-  var brush = document.getElementById('brush');
+.colors button:nth-of-type(1) {
+  background-color: #FBFF00;
+}
 
-  brush.addEventListener('input', function(brush){
-    context.lineWidth = brush.target.value;
-  });
+.colors button:nth-of-type(2) {
+  background-color: #009fff;
+}
 
-  // Handle Colors
-  var colors = document.getElementsByClassName('colors')[0];
+.colors button:nth-of-type(3) {
+  background-color: #000000;
+}
 
-  colors.addEventListener('click', function(event) {
-    context.strokeStyle = event.target.value || 'black';
-  });
+.colors button:nth-of-type(4) {
+  background-color: #FF0000;
+}
 
+.colors button:nth-of-type(5) {
+  background-color: #3EFF00;
+}
 
-  // Mouse Down Event
-  canvas.addEventListener('mousedown', function(event) {
-    setMouseCoordinates(event);
-    isDrawing = true;
-
-    // Start Drawing
-    context.beginPath();
-    context.moveTo(mouseX, mouseY);
-  });
-
-  // Mouse Move Event
-  canvas.addEventListener('mousemove', function(event) {
-    setMouseCoordinates(event);
-
-    if(isDrawing){
-      context.lineTo(mouseX, mouseY);
-      context.stroke();
-    }
-  });
-
-  // Mouse Up Event
-  canvas.addEventListener('mouseup', function(event) {
-    setMouseCoordinates(event);
-    isDrawing = false;
-  });
-
-  // Handle Mouse Coordinates
-  function setMouseCoordinates(event) {
-    mouseX = event.clientX - boundings.left;
-    mouseY = event.clientY - boundings.top;
-  }
-
-  // Handle Clear Button
-  var clearButton = document.getElementById('clear');
-
-  clearButton.addEventListener('click', function() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-  });
-
-  // Handle Save Button
-  var saveButton = document.getElementById('save');
-
-  saveButton.addEventListener('click', function() {
-    var imageName = prompt('Please enter image name');
-    var canvasDataURL = canvas.toDataURL();
-    var a = document.createElement('a');
-    a.href = canvasDataURL;
-    a.download = imageName || 'drawing';
-    a.click();
-  });
-  
-};
-
+.colors button:nth-of-type(6) {
+  background-color: #FFF;
+}
